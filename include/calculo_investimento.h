74f2a1536426ec0986e8abf1913f56d4905db443
MoLittle <162193110+MoLittle@users.noreply.github.com>
2025-02-10 21:59:50 -0300

    Update calculo_investimento.h

--- include/calculo_investimento.h
+++ include/calculo_investimento.h
@@ -1,1 +1,11 @@
+#ifndef CALCULO_INVESTIMENTO_H
+#define CALCULO_INVESTIMENTO_H
+
+#include "investimento.h"
+
+void obterDataAtual(char *dataAtual);
+int calcularDiferencaDias(const char *dataInicio, const char *dataFim);
+void atualizarInvestimento(Investimento *investimento, const char *dataAtual);
+
+#endif // CALCULO_INVESTIMENTO_H
 
